d02f81a82fbec79af0eb734598b2d21d1d4091af
developerankit1 <ankitmaurya9560@gmail.com>
2024-12-19 17:14:42 +0530

    Faltu Code

--- Week 3/index.js
+++ Week 3/index.js
@@ -1,5 +1,20 @@
+let ctr = 1;
 function addTodo() {
   const inputEl = document.querySelector("input");
   const value = inputEl.value;
-  console.log(value);
+
+  const divEl = document.createElement("div");
+  divEl.innerHTML =
+    "<div>" +
+    value +
+    "</div><button onclick='deleteTodo(' + ctr + ')'>Delete</button>";
+  divEl.setAttribute("id", ctr);
+  ctr = ctr + 1;
+
+  document.querySelector("body").appendChild(divEl);
+}
+
+function deleteTodo(index) {
+  const element = document.getElementById(index);
+  element.parentNode.removeChild(element);
 }
