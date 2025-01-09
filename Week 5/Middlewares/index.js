ddba002e9ee4c2e2d76214fc19672cb5d72ee3e2
developerankit1 <ankitmaurya9560@gmail.com>
2025-01-09 15:31:18 +0530

    MIDDLEWARE ASSIGNMENTS

--- Week 5/Middlewares/index.js
+++ Week 5/Middlewares/index.js
@@ -1,17 +1,50 @@
+// const express = require("express");
+
+// const app = express();
+
+// let requestcount = 0;
+
+// function requestIncreaser(req, res, next) {
+//   requestcount = requestcount + 1;
+//   console.log(`Total number of requests ${requestcount}`);
+
+//   next();
+// }
+
+// app.get("/sum", requestIncreaser, function (req, res) {
+//   const a = parseInt(req.query.a);
+//   const b = parseInt(req.query.b);
+
+//   res.json({
+//     ans: a + b,
+//   });
+// });
+
+// app.get("/multiply", requestIncreaser, function (req, res) {
+//   const a = req.query.a;
+//   const b = req.query.b;
+//   res.json({
+//     ans: a * b,
+//   });
+// });
+
+// app.listen(3000, () => {
+//   console.log("server is listening on port 3000");
+// });
+
 const express = require("express");
 
 const app = express();
+// log method , url , timestmap
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl;
 
-let requestcount = 0;
-
-function requestIncreaser(req, res, next) {
-  requestcount = requestcount + 1;
-  console.log(`Total number of requests ${requestcount}`);
-
+  console.log(`[${timestamp}] ${method} ${url}`);
   next();
-}
-
-app.get("/sum", requestIncreaser, function (req, res) {
+});
+app.get("/sum", function (req, res) {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
 
@@ -20,14 +53,28 @@ app.get("/sum", requestIncreaser, function (req, res) {
   });
 });
 
-app.get("/multiply", requestIncreaser, function (req, res) {
-  const a = req.query.a; 
+app.get("/multiply", function (req, res) {
+  const a = req.query.a;
   const b = req.query.b;
   res.json({
     ans: a * b,
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is listening on port 3000");
+app.get("/divide", function (req, res) {
+  const a = req.query.a;
+  const b = req.query.b;
+  res.json({
+    ans: a / b,
+  });
 });
+
+app.get("/subtract", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  res.json({
+    ans: a - b,
+  });
+});
+
+app.listen(3000);
