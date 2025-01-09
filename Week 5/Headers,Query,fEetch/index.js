e7ed4fad127bc9d31a9af58ec36739033ac6e3b4
developerankit1 <ankitmaurya9560@gmail.com>
2025-01-09 12:31:01 +0530

    axios lib

--- Week 5/Headers,Query,fEetch/index.js
+++ Week 5/Headers,Query,fEetch/index.js
@@ -1,7 +1,14 @@
-const fetchPost = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const json = await res.json();
-  document.getElementById("posts").innerHTML = json.body;
+// const fetchPost = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//   const json = await res.json();
+//   document.getElementById("posts").innerHTML = json.body;
+// };
+
+// fetchPost();
+
+const fetchpost = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+  document.getElementById("posts").innerHTML = res.data.body;
 };
 
-fetchPost();
+fetchpost();
