5fdca45e63eb1d1b0c4ca56b3d72f9a3c4fd823e
developerankit1 <ankitmaurya9560@gmail.com>
2025-01-21 10:47:16 +0530

    Intial Files using MONGODB

---
+++ Week 7/Mongo_DB/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup", (req, res) => {});
+
+app.post("/login", (req, res) => {});
+
+app.post("/todo", (req, res) => {});
+
+app.get("/todos", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
