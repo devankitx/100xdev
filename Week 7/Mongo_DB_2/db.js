851fa8e859e8f9956c12033183f84ec7ae693146
developerankit1 <ankitmaurya9560@gmail.com>
2025-01-23 13:25:34 +0530

    Backend Done with mongodb

--- Week 7/Mongo_DB_2/db.js
+++ Week 7/Mongo_DB_2/db.js
@@ -9,10 +9,7 @@ const Schema = mongoose.Schema;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB!"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
