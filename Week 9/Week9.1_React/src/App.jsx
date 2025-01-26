c0ab02e3f0dafd755bcef20fe2fa86180efe9bc2
developerankit1 <ankitmaurya9560@gmail.com>
2025-01-26 09:34:50 +0530

    stopwatch using useffect hook

--- Week 9/Week9.1_React/src/App.jsx
+++ Week 9/Week9.1_React/src/App.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return <div>{seconds} seconds elapsed</div>;
+};
+
 function App() {
   return (
     <div>
-      <h1>Making this changes to maintain github sreak sorry future ankit i was not able to code today because i was distracted by manythings bye</h1>
+      <h1>Timer App</h1>
+      <Timer />
     </div>
   );
 }
