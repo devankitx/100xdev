c722646deb669ee79d4644a92042a2d105ce4724
devankitx <ankitmaurya9560@gmail.com>
2025-02-16 09:23:23 +0530

    not working the prop driiling

--- Week 10/React_Part_1/src/App.jsx
+++ Week 10/React_Part_1/src/App.jsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 
-function App() {
+// LightBulb Component
+const LightBulb = ({ isOn }) => {
+  return <div>The light is {isOn ? "ON" : "OFF"}</div>;
+};
+
+// LightSwitch Component
+const LightSwitch = ({ toggleLight }) => {
+  return <button onClick={toggleLight}>Toggle Light</button>;
+};
+
+// App Component
+const App = () => {
+  const [isLightOn, setIsLightOn] = useState(false);
+
+  const toggleLight = () => {
+    setIsLightOn((prev) => !prev);
+  };
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div>
+      <LightBulb isOn={isLightOn} />
+      <LightSwitch toggleLight={toggleLight} />
+    </div>
   );
-
-  function Home() {
-    return <h1>Home</h1>;
-  }
-  function About() {
-    return <h1>About</h1>;
-  }
-}
+};
 
 export default App;
