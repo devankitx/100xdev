5ac5327ab4b3bea7e6203a89da15197fbea13ac5
devankitx <126304960+devankitx@users.noreply.github.com>
2025-03-01 12:25:40 +0530

    typescripts

--- Week 14/typescript/index.js
+++ Week 14/typescript/index.js
@@ -1,5 +1,12 @@
 "use strict";
-function sum(a, b) {
-    console.log(a + b);
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
+// let ans = sum(1, 4);
+// console.log(ans);
+function delayedCall(fn) {
+    setTimeout(fn, 3000);
 }
-sum(1, 4);
+delayedCall(() => {
+    console.log("hi there");
+});
--- Week 14/typescript/index.ts
+++ Week 14/typescript/index.ts
@@ -1,5 +1,14 @@
-function sum(a: number, b: number) {
-  console.log(a + b);
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
+
+// let ans = sum(1, 4);
+// console.log(ans);
+
+function delayedCall(fn: () => void) {
+  setTimeout(fn, 3000);
 }
 
-sum(1, 4);
+delayedCall(() => {
+  console.log("hi there");
+});
