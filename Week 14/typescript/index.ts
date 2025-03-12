fc4ef39885416466fc7ff20d12ae2758064b51fa
devankitx <126304960+devankitx@users.noreply.github.com>
2025-03-12 08:53:59 +0530

    types and interface

--- Week 14/typescript/index.ts
+++ Week 14/typescript/index.ts
@@ -1,47 +1,9 @@
-// function sum(a: number, b: number): number {
-//   return a + b;
-// }
-
-// let ans = sum(1, 4);
-// console.log(ans);
-
-// function delayedCall(fn: () => void) {
-//   setTimeout(fn, 3000);
-// }
-
-// delayedCall(() => {
-//   console.log("hi there");
-// });
 interface User {
   name: string;
   age: number;
-  address?: {
-    city: string;
-    country: string;
-    pincode: number;
-  };
-}
-let user: User = {
-  name: "sachin",
-  age: 17,
-  address: {
-    city: "pune",
-    country: "india",
-    pincode: 411045,
-  },
-};
-let user2: User = {
-  name: "sachin",
-  age: 17,
-};
-
-function islegalToVote(user: User): boolean {
-  if (user.age >= 18) {
-    return true;
-  } else {
-    return false;
-  }
 }
 
-let ans = islegalToVote(user);
-console.log(ans);
+interface Admin {
+  name: string;
+  permission: string,
+}
