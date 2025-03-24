bb14c8f571a3b187f9a115d6077af28caf37b09c
devankitx <126304960+devankitx@users.noreply.github.com>
2025-03-24 11:44:51 +0530

    partial api typescirpt

--- Week 14/week14.2/src/index.ts
+++ Week 14/week14.2/src/index.ts
@@ -1,13 +1,16 @@
 interface User {
-  id: number;
+  id: string;
   name: string;
+  age: string;
   email: string;
-  createdAt: Date;
+  password: string;
 }
 
-// For a profile display, only pick `name` and `email`
-type UserProfile = Pick<User, "name" | "email">;
+type UpdateProps = Pick<User, "name" | "age" | "email">;
 
-const displayUserProfile = (user: UserProfile) => {
-  console.log(`Name: ${user.name}, Email: ${user.email}`);
-};
+type UpdatePropsOptional = Partial<UpdateProps>;
+
+function updateUser(updatedProps: UpdatePropsOptional) {
+  // hit the database tp update the user
+}
+updateUser({});
